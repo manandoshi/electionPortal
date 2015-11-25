3fe3b0e468e57c12b11b1d8b98a8de3bfe690232
Harsh Sheth <harsheth96@gmail.com>
2015-11-25 20:23:09 +0530

    Fix stuff

--- scripts.js
+++ scripts.js
@@ -4,7 +4,13 @@ var submit =function() {
   var MInum=$('#mi_number').val();
   var data = {name_of_team : name, logoID : logo, mi_number : MInum}
   $.post( '/teamdata', data, function(recv) {
-         console.log(recv);
+         if(recv[error] == 0)
+         {
+            alert("Team Added Successfully");
+            $('#name_of_team').val("");
+            $('#logoID').val("");
+            $('#mi_number').val("");
+         }
        },
        'json' // I expect a JSON response
     );

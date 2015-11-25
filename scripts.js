cf7bb02e5f571192cb9aa6564ea413918c46a81d
Harsh Sheth <harsheth96@gmail.com>
2015-11-25 22:52:57 +0530

    6 kar diya

--- scripts.js
+++ scripts.js
@@ -18,7 +18,6 @@ var submitreg =function() {
   var name=$('#name_of_team').val();
   var MInum=$('#mi_number').val();
   var data = {name_of_team : name, logoID : logo, mi_number : MInum};
-  console.log(data);
   $.post( '/teamdata', data, function(recv) {
          if(recv["error"] == 0)
          {
